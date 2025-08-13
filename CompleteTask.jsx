8ccbd471f94297a162491b2781a762c8df6038b5
anishjii123-hash <anishjii123@gmail.com>
2025-08-13 21:05:38 +0530

    Add files via upload

---
+++ CompleteTask.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function CompleteTask({data}) {
+  return (
+    <div>
+        <div className=' flex-shrink-0 h-full p-5 w-[300px] bg-blue-400 rounded-xl'>
+<div className='flex justify-between text-center'>
+    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+     <h4 className='text-sm'>{data.data}</h4>
+</div>
+<h2 className='mt-10 font-semibold text-2xl '>{data.title}</h2>
+<p className='mt-2'>{data.description}</p>
+      <div className=' mt-2'>
+        <button className='w-full bg-green-700 rounded-2xl'> Completed </button>
+
+      </div>
+        </div>
+    </div>
+  )
+}
+
+export default CompleteTask
